b6f789a43a7ed70ef1d0a5f298b531f0e5cd5aee
Bryscar20 <brysonosc20@gmail.com>
2020-11-04 16:54:54 +0300

    authentication

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Pre-course work -
 ####################################
 1. Link google fonts to html
+2. User & session authentication
--- pre-course/run.py
+++ pre-course/run.py
@@ -1,18 +1,44 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__, root_path="C:\\Users\\adladmin\\Desktop\\pYTHON_pROG\\Newfolder\\PythonNetwork\\Newfolder\\curriculum\\Folder_1\\Workplace\\Workplace_init\\pre-course\\my_root_Dir")
+app.secret_key = 'somesecretkeythatonlyishouldknow'
 
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='test1@123.com', password='test123'))
+users.append(User(id=2, username='test2', password='test234'))
+users.append(User(id=3, username='test3', password='test345'))
+
+# Homepage
 @app.route("/", methods=['GET','POST'])
 def index():
     return render_template("index.html")
-
+# Profile page
 @app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        session.pop('user_id', None)
 
-@app.route("/register", methods=['GET','POST'])
-def register():
-    return render_template("register.html")
+        username = request.form['username']
+        password = request.form['password']
+    
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return render_template("profile.html")
+    return render_template("index.html")
+
+@app.route("/profile", methods=['GET','POST'])
+def profile():
+    return render_template("profile.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
