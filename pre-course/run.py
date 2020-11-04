acb8e243c4d0d423d0d72b12503c7006941e9826
Bryscar20 <brysonosc20@gmail.com>
2020-11-04 16:14:49 +0300

    update login

--- pre-course/run.py
+++ pre-course/run.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__, root_path="C:\\Users\\adladmin\\Desktop\\pYTHON_pROG\\Newfolder\\PythonNetwork\\Newfolder\\curriculum\\Folder_1\\Workplace\\Workplace_init\\pre-course\\my_root_Dir")
-# app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET','POST'])
 def index():
-    """
-    docstring
-    """
     return render_template("index.html")
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    return render_template("login.html")
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
