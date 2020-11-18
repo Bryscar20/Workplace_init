80187217ab011fc3aec1d54d09653c7f44300677
Bryscar20 <brysonosc20@gmail.com>
2020-11-18 16:56:25 +0300

    Grids and Layouts

--- pre-course/auth_app/main.py
+++ pre-course/auth_app/main.py
@@ -1,21 +1,12 @@
 from flask import Flask,render_template,flash, redirect,url_for,session,logging,request
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '81920bd700541c2643206d1097bfdea4a781dfb7'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
-
-
-class user(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    email = db.Column(db.String(120))
-    password = db.Column(db.String(80))
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", n_data="n_data", name="Admin")
+    title = "WELCOME - HomePage"
+    return render_template("index.html", n_data="n_data", name="Admin", title=title)
 
 @app.route("/login",methods=["GET", "POST"])
 def login():
@@ -29,7 +20,18 @@ def register():
     
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    title = "WELCOME - About Page"
+    return render_template("about.html", title=title)
+
+@app.route("/services")
+def services():
+    title = "WELCOME - Services Page"
+    return render_template("services.html", title=title)
+
+@app.route("/services")
+def services():
+    title = "WELCOME - Contact Page"
+    return render_template("services.html", title=title)
     
 if __name__ == "__main__":
     app.run(debug=True)
